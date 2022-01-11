c4e7e36c4d500aa391ead201c2a37c54196a36f0
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-11 15:37:19 +0100

    commit

--- lib/app/config/app_network.dart
+++ lib/app/config/app_network.dart
@@ -1,17 +1,17 @@
-import 'package:connectivity/connectivity.dart';
-
-class Network {
-  static get hasConnection async {
-    var result = await (Connectivity().checkConnectivity());
-    switch (result) {
-      case ConnectivityResult.wifi:
-        return true;
-      case ConnectivityResult.mobile:
-        return true;
-      case ConnectivityResult.none:
-        return false;
-      default:
-        return false;
-    }
-  }
-}
+// import 'package:connectivity/connectivity.dart';
+//
+// class Network {
+//   static get hasConnection async {
+//     var result = await (Connectivity().checkConnectivity());
+//     switch (result) {
+//       case ConnectivityResult.wifi:
+//         return true;
+//       case ConnectivityResult.mobile:
+//         return true;
+//       case ConnectivityResult.none:
+//         return false;
+//       default:
+//         return false;
+//     }
+//   }
+// }
