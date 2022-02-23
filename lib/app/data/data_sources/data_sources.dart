0da6aa4f69e5ac992eac892115d5965dac592315
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-02-23 21:28:26 +0100

    commit

--- lib/app/data/data_sources/data_sources.dart
+++ lib/app/data/data_sources/data_sources.dart
@@ -88,8 +88,6 @@ class DataSources extends GetConnect {
         await db.execute(_tbl_transaction_query);
 
         ///
-        ///
-        ///
         await db.execute(_tbl_settings_data_query);
         await db.execute(_tbl_category_data_query);
         await db.execute(_tbl_transaction_data_query);
